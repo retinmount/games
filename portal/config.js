b8be60908d50f45590838cf6744fca00c4702b27
retinmount <240333300+retinmount@users.noreply.github.com>
2025-10-26 23:43:06 +0300

    upload files

---
+++ portal/config.js
@@ -0,0 +1,13 @@
+window.CONFIG = {
+	debug: false,
+	version: '1.0.5321',
+	GA_ACCOUNT: 'UA-106829086-1',
+	GA_ADS_ACCOUNT: 'UA-122747211-1',
+	gameTitle: 'portal',
+	adsConfigUrl: 'http://apps.tvgam.es/tv_games/config/production/config.json',
+	baseUrl: '',
+	disabled_exit_url: 'exit_url_not_used',
+	customer: "",
+	env: "production",
+	locale: "en",
+};
---
+++ zebra_crossing/config.js
@@ -0,0 +1,13 @@
+window.CONFIG = {
+	debug: false,
+	version: '1.0.5321',
+	GA_ACCOUNT: 'UA-106829086-1',
+	GA_ADS_ACCOUNT: 'UA-122747211-1',
+	gameTitle: 'Zebra Crossing',
+	adsConfigUrl: 'http://apps.tvgam.es/tv_games/config/production/config.json',
+	baseUrl: '',
+	exitUrl: '../portal/index.html#zebraCrossing',
+	customer: "",
+	env: "production",
+	locale: "en",
+};
